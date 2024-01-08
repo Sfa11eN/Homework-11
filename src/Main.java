0a20a4a52e64252312f5ce590daecacde42d6b44
Sfa11eN <sfallenn@gmail.com>
2024-01-08 13:58:06 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+import java.time.LocalDate;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Тема Методы");
+        System.out.println("Задание 1");
+        yearCheck(1991);
+        appVersion(1, 2019);
+        cardDeliveryDays(740);
+
+    }
+
+    public static void taskStrings() {
+    }
+
+    public static void yearCheck(int year) {
+        if (year >= 1584 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)) {
+            System.out.println("Год високосный");
+        } else {
+            System.out.println("Год не является високосным.");
+
+        }
+        System.out.println("Задание 2");
+    }
+
+    public static void appVersion(int mobileOs, int mobileYear) {
+        String mobileOsName;
+        switch (mobileOs) {
+            case 0:
+                mobileOsName = "iOs";
+                break;
+            case 1:
+                mobileOsName = "Android";
+                break;
+            default:
+                mobileOsName = "Неподдерживаемая ОС";
+
+        }
+        int currentYear = LocalDate.now().getYear();
+        if (mobileYear != currentYear) {
+            System.out.println(" Для ОС " + mobileOsName + " установите Lite версию ");
+        } else {
+            System.out.println(" Для ОС " + mobileOsName + " установите обычную версию ");
+        }
+        System.out.println("Задание 3");
+    }
+
+    public static int printCardDeliveryDays(int distance) {
+        int days = cardDeliveryDays(distance);
+        if (days > 0) {
+            System.out.println("Потребуется дней" + days);
+        } else {
+            System.out.println("Доставка не осуществляется");
+        }
+
+
+        public static int cardDeliveryDays ( int distance){
+            if (distance <= 20) {
+                return 1;
+            } else if (distance <= 60) {
+                return 2;
+            } else if (distance <= 100) {
+                return 3;
+            } else {
+                return -1;
+            }
+        }
+
+    }
+}
